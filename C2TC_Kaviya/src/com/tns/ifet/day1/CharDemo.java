23d8f7bd0cc5502b69a54da23c05204a6456febc
KaviyaMohandass <bharathikaviya18@gmail.com>
2024-10-25 20:45:00 +0530

    third

---
+++ C2TC_Kaviya/src/com/tns/ifet/day1/CharDemo.java
@@ -0,0 +1,19 @@
+package com.tns.ifet.day1;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char ch1='a';
+		System.out.println(ch1);
+		char ch2=65;
+		System.out.println(ch2);
+		char var1='\u00A7';
+		System.out.println(var1);
+		char var2='\u20AC';
+		System.out.println(var2);
+		int a='A';//  if char has been given means its ACCII
+		System.out.println(a);
+
+	}
+
+}
