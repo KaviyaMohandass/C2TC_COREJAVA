060c64ae326ee1dc6a92afa198c772cd3a65065a
KaviyaMohandass <bharathikaviya18@gmail.com>
2024-10-25 15:14:34 +0530

    second

--- C2TC_Kaviya/src/com/tns/ifet/day1/FirstPrograme.java
+++ C2TC_Kaviya/src/com/tns/ifet/day1/FirstPrograme.java
@@ -3,8 +3,9 @@ package com.tns.ifet.day1;
 public class FirstPrograme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello Kaviya");
+		// TODO Auto-generated method stubint
+		int number=20;
+		System.out.println(" value of number:"+number);
 	}
 
 }
