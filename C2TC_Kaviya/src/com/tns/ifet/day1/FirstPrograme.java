6f0a699598f589c05a43c760b0f8b9b623515b63
KaviyaMohandass <bharathikaviya18@gmail.com>
2024-10-25 14:31:59 +0530

    initial

---
+++ C2TC_Kaviya/src/com/tns/ifet/day1/FirstPrograme.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day1;
+
+public class FirstPrograme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Kaviya");
+	}
+
+}
